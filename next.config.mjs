b744408094fd026e243fa65203a28985d36ce0e0
lTriNiTyl <antoniokr@gmail.com>
2024-03-08 18:27:29 +0900

    "Use external image urls"

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // 외부에서 이미지를 가져올 수 있도록 설정. [
+  images: {
+    remotePatterns:[
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      }
+    ]
+  }
+  // ]
+};
 
 export default nextConfig;
