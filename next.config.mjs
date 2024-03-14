32131f79d7f7be93b8c6a92dd9579701aed9c9a9
lTriNiTyl <antoniokr@gmail.com>
2024-03-15 00:27:01 +0900

    "SVG파일 추가 및 SVG 사용에 필요한 모듈 설치 및 config 추가"

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 웹팩을 통해 svg 파일을 리액트 컴포넌트로 변환하는 설정이다
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: "@svgr/webpack", options: { icon: true } }],
+    });
+
+    return config;
+  },
   // 외부에서 이미지를 가져올 수 있도록 설정. [
   images: {
-    remotePatterns:[
+    remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.pexels.com',
---
+++ public/svg/index.ts
@@ -0,0 +1,6 @@
+export { default as HomeIcon } from './home.svg';
+export { default as AboutIcon } from './about.svg';
+export { default as ContactIcon } from './contact.svg';
+export { default as BlogIcon } from './blog.svg';
+export { default as LoginIcon } from './login.svg';
+
