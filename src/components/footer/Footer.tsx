1f77adcf4abb27af75d3c63bbb1cd67f691846b8
lTriNiTyl <antoniokr@gmail.com>
2024-03-19 21:14:38 +0900

    "modified text on footer"

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="flex justify-between items-center h-[100px] text-gray-400 max-md:justify-around max-md:flex-col max-md:items-center">
-      <div className="font-semibold">lamadev</div>
+      <div className="font-semibold">TriNiTy</div>
         <div className="text-xs">
           Lama creative thoughts agency @ All rights reserved.
         </div>
